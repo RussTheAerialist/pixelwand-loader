35baeeb552c70b3d372687ea285860e2fde4db3f
Russell Hay <me@russellhay.com>
2013-11-06 20:24:09 -0800

    Image creation is done, need to add PySerial into the mix now

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pixelwand-loader',
+    version='1.0',
+    packages=['pwl'],
+    url='http://cordandruss.com/',
+    license='MIT LICENSE',
+    author='Russell Hay',
+    author_email='me@russellhay.com',
+    description='An Image Loader for the PixelWand'
+)
